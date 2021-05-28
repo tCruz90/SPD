b24749ca0e1d873dd7801985aab17721417377bb
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-28 17:05:57 +0100

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 #STATICFILES_DIRS = [BASE_DIR / "static",]
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+#STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
