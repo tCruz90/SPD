009678ae975c16e11d73e3e957e5cd5272e0c80d
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-29 15:17:25 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-gunicorn --workers 8 --threads 4 --timeout 1200 --bind=0.0.0.0:8080 -k uvicorn.workers.UvicornWorker \ --chdir=ecommerce/ecommerce ecommerce.asgi
+gunicorn --workers 8 --threads 4 --timeout 1200 --bind=0.0.0.0:8080 -k uvicorn.workers.UvicornWorker \ --chdir=/ecommerce/ecommerce ecommerce.asgi
