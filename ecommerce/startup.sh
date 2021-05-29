44880ecf534f1aa3c110f956ad1631f4da8911c5
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-29 19:14:09 +0100

    Update startup.sh

--- ecommerce/startup.sh
+++ ecommerce/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ecommerce
-pip3 install -r requirements.txt && gunicorn -w 2 -b 0.0.0.0:8000 ecommerce.wsgi --reload --timeout 900
+pip3 install -r requirements.txt && gunicorn -w 2 -b 0.0.0.0 ecommerce.wsgi
