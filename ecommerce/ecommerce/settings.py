a570b05319c19e0c06f0191350049dd6f27fca5c
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-30 16:04:34 +0100

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
