81ef859e1840ef8e8a3a61503f6887eac81e623c
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-30 17:11:49 +0100

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'whitenoise.runserver_nostatic',
     'store.apps.StoreConfig',
 ]
 
@@ -53,7 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    # Add whitenoise middleware after the security middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -125,21 +126,32 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-#STATIC_ROOT = "ecommerce/static"
+
+"""
+STATIC_ROOT = '/static/'
 
 STATIC_URL = '/static/'
+"""
+
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
 
 MEDIA_URL = '/images/'
 
-#STATICFILES_DIRS = [BASE_DIR / "static",]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+#STATICFILES_DIRS = [BASE_DIR / "static",]
+"""
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+"""
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage')
+
+
+
+
 
 
 # Default primary key field type
